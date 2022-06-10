62b32223f5d3202ac4dd61a7da076586843919d2
Miroslav Peterka <wiki@atlas.cz>
2022-06-10 14:18:23 +0200

    set base path also in router

--- src/router.js
+++ src/router.js
@@ -35,7 +35,9 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(
+    process.env.NODE_ENV === 'production' ? '/apptemplate/' : '/'
+  ),
   routes,
 })
 
